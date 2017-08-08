7bc9366828a0224999868e8dcf1dbfbf566b3b68
mothermoose <melanie.ilseman@gmail.com>
2017-08-07 20:28:52 -0500

    created registration form for resident

--- app/controllers/residents_controller.rb
+++ app/controllers/residents_controller.rb
@@ -6,21 +6,34 @@ class ResidentsController < ApplicationController
   end 
 
   def new
+    render "new.html.erb"
   end 
 
   def create
+    resident = Resident.create(
+                first_name: params[:first_name].capitalize,
+                last_name: params[:last_name].capitalize,
+                date: params[:date],
+                hmis_number: params[:hmis_number],
+                hmis_entry_date: params[:hmis_entry_date],
+                documented: params[:documented],
+                gender: params[:gender],
+                ethnicity: params[:ethnicity]
+              )
+
+    redirect_to '/residents'
   end 
 
-  def show
-  end 
+#   def show
+#   end 
 
-  def edit
-  end 
+#   def edit
+#   end 
 
-  def update
-  end 
+#   def update
+#   end 
 
-  def destroy
-  end 
+#   def destroy
+#   end 
 
 end
