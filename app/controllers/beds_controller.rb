860b8f25a2b6564852861d84cf9d0f211ef138b2
Kyle Gundry <Gundr1kt@gmail.com>
2017-08-07 19:34:23 -0500

    Add show page for bed

--- app/controllers/beds_controller.rb
+++ app/controllers/beds_controller.rb
@@ -4,4 +4,9 @@ class BedsController < ApplicationController
     @beds = Bed.all
     render "index.html.erb"
   end
+
+  def show
+    @bed = Bed.find_by(id: params[:id])
+    render "show.html.erb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get "/" => "application#index"
 
   get "/beds" => "beds#index"
+  get "/beds/:id" => "beds#show"
 
 end
