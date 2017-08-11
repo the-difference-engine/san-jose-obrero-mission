0feec4813941497cfa115bcfb8382b80b5827ca1
mothermoose <melanie.ilseman@gmail.com>
2017-08-10 19:52:05 -0500

    deleted old ActiveRecord

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 
 ActiveRecord::Schema.define(version: 20170811003137) do
-ActiveRecord::Schema.define(version: 20170808001946) do
+# ActiveRecord::Schema.define(version: 20170808001946) do
 
 
   # These are extensions that must be enabled in order to support this database
