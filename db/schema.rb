5020c51c98c555e29c79ba24de043325ca598532
Kevin <kevingm31@gmail.com>
2017-09-07 18:43:04 -0500

    No more Devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170831235331) do
+ActiveRecord::Schema.define(version: 20170901014334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,40 +23,6 @@ ActiveRecord::Schema.define(version: 20170831235331) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.integer  "resident_id"
-  end
-
-  create_table "case_managers", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_case_managers_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_case_managers_on_reset_password_token", unique: true, using: :btree
-  end
-
-  create_table "residential_aids", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_residential_aids_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_residential_aids_on_reset_password_token", unique: true, using: :btree
   end
 
   create_table "residents", force: :cascade do |t|
@@ -78,23 +44,6 @@ ActiveRecord::Schema.define(version: 20170831235331) do
     t.integer  "number_of_shelters"
     t.boolean  "chronically_homeless"
     t.string   "image"
-  end
-
-  create_table "securities", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_securities_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_securities_on_reset_password_token", unique: true, using: :btree
   end
 
   create_table "users", force: :cascade do |t|
