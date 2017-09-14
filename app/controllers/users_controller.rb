c0065d2134b91c46aba9bbaebff3d58e18b8b422
Troy Leach <troyleach29@gmail.com>
2017-09-14 08:51:41 -0500

    added whitelisted params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,8 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(
-      email: params[:email],
-      role: params[:role],
-      password: params[:password],
-      password_confirmation: params[:password_confirmation]
-    )
-    if user.save
+    user = User.new(user_params)
+    if user.save!
       session[:user_id] = user.id
       flash[:success] = 'Successfully created account!'
       redirect_to '/'
@@ -20,5 +15,11 @@ class UsersController < ApplicationController
       flash[:warning] = 'Invalid email or password!'
       redirect_to '/signup'
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :role, :password, :password_confirmation)
   end
 end
