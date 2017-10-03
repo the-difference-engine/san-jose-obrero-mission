99fe21e13fa6e00260c6da969be2d4b66c4a561f
Kyle Gundry <Gundr1kt@gmail.com>
2017-10-02 20:10:39 -0500

    Added API and Serializer

---
+++ app/controllers/api/v1/api_controller.rb
@@ -0,0 +1,7 @@
+module Api
+  module V1
+    class ApiController < ApplicationController
+
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -23,5 +23,9 @@ Rails.application.routes.draw do
   post "/login" => "sessions#create"
   get "/logout" => "sessions#destroy"
 
-
+  namespace :api, defaults: { format: 'json' } do
+    namespace :v1 do
+      resources :users, only: [:index, :create, :show, :update, :destroy]
+    end
+  end
 end
