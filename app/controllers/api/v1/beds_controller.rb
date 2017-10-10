d9a7570aff06fd059430c9efd3fff95c435e078e
mothermoose <melanie.ilseman@gmail.com>
2017-10-09 20:07:57 -0500

    added update, create, destroy actions to api controller

--- app/controllers/api/v1/beds_controller.rb
+++ app/controllers/api/v1/beds_controller.rb
@@ -2,6 +2,8 @@ module Api
   module V1
     class BedsController < Api::V1::BaseController
 
+      before_action :set_bed, only: [:show, :update, :destroy]
+
       def index
         render json: Bed.all
       end
@@ -9,6 +11,32 @@ module Api
       def show 
         bed = Bed.find(params[:id])
         render json: bed
+      end  
+
+      def create
+        bed = Bed.create!(bed_params)
+        render json: bed
+      end 
+
+      def update
+        @bed.update(bed_params)
+        render json: @bed
+      end 
+
+      def destroy
+        @bed.destroy
+        render json: @bed
+      end 
+
+      private
+
+      def bed_params
+        #whitelist params
+        params.permit(:name, :top_or_bottom, :occupied)
+      end 
+
+      def set_bed
+        @bed = Bed.find(params[:id])
       end 
     
     end
