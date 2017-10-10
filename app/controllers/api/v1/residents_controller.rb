31adad4e1503f6fc71c4a2ead92b6e014e9e1017
Fred Calhoun <fcalhoun4@yahoo.com>
2017-10-09 19:57:23 -0500

    wip

--- app/controllers/api/v1/residents_controller.rb
+++ app/controllers/api/v1/residents_controller.rb
@@ -5,21 +5,28 @@ module Api
         # GET /v1/users
         def index
           render json: Resident.all
+        end
 
         def create
+          @resident = Resident.create!(resident_params)
+          json_response(@resident, :created)
         end
 
 
   
         def show
-          Resident = Resident.find(params[:id])
-          render json: resident
+          # Resident = Resident.find(params[:id])
+          json_response(@resident)
         end
 
         def update
+          @resident.update(resident_params)
+          head :no_content
         end
 
         def destroy
+          @resident.destroy
+          head :no_content
         end
       end
     end
