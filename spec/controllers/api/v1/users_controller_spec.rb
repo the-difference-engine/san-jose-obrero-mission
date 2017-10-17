36b269aa86852060b6203822bc9252fa95c99753
Kyle Gundry <Gundr1kt@gmail.com>
2017-10-16 19:09:17 -0500

    Removed file without spec in name

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,20 +1,25 @@
 require 'rails_helper'
+include RequestSpecHelper
 
-RSpec.describe UsersController, type: :controller do
-  describe "GET 'index' " do
-    it "returns a successful 200 response" do
+# TODO these tests will fail ***
+
+RSpec.describe Api::V1::UsersController, type: :controller do
+  describe 'GET#index' do
+    it 'returns http success' do
       get :index, format: :json
       expect(response).to have_http_status(200)
     end
 
-    it "to return 3 users" do
-      User.create(email: "test1@email1.com", role: "admin", password: "password")
-      User.create(email: "test1@email2.com", role: "security", password: "password")
-      User.create(email: "test1@email3.com", role: "case_manager", password: "password")
+    it 'expect to return 3 users' do
+      create(:user,
+             role: 1)
+      create(:admin_user)
+
       get :index, format: :json
       json = JSON.parse(response.body)
-      expect(json.size).to eq(3)
-      expect(json)
+      expect(json).not_to be_empty
+      expect(json.size).to eq(4)
+      expect(json[1]["email"]).to eq('test2@email.com')
     end
   end
 end
