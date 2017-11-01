be010a3e95abd0d912dbeadc1a7f2c6c89897267
Troy Leach <troyleach29@gmail.com>
2017-11-01 12:34:07 -0500

    fixed stashing conflicts

--- app/assets/javascripts/beds.js
+++ app/assets/javascripts/beds.js
@@ -1,328 +1,3 @@
-
-
-var bedsPayload = {
-  "beds": [
-
-    {
-      "name": "No statement can catch the ChuckNorrisException.",
-      "top_or_bottom": "bottom",
-      "occupied": false
-    },
-    {
-      "name": "Chuck Norris' programs occupy 150% of CPU, even when they are not executing.",
-      "top_or_bottom": "bottom",
-      "occupied": false
-    },
-    {
-      "name": "Project managers never ask Chuck Norris for estimations... ever.",
-      "top_or_bottom": "top",
-      "occupied": true
-    },
-    {
-      "name": "Chuck Norris doesn't need an OS.",
-      "top_or_bottom": "bottom",
-      "occupied": false
-    },
-    {
-      "name": "For Chuck Norris, NP-Hard = O(1).",
-      "top_or_bottom": "top",
-      "occupied": true
-    },
-    {
-      "name": "Chuck Norris can spawn threads that complete before they are started.",
-      "top_or_bottom": "bottom",
-      "occupied": false
-    },
-    {
-      "name": "Chuck Norris can't test for equality because he has no equal.",
-      "top_or_bottom": "top",
-      "occupied": true
-    },
-    {
-      "name": "When Chuck Norris points to null, null quakes in fear.",
-      "top_or_bottom": "bottom",
-      "occupied": false
-    },
-    {
-      "name": "\"It works on my machine\" always holds true for Chuck Norris.",
-      "top_or_bottom": "top",
-      "occupied": true
-    },
-    {
-      "name": "Chuck Norris programs do not accept input.",
-      "top_or_bottom": "bottom",
-      "occupied": false
-    },
-    {
-      "name": "\"It works on my machine\" always holds true for Chuck Norris.",
-      "top_or_bottom": "top",
-      "occupied": true
-    },
-    {
-      "name": "For Chuck Norris, NP-Hard = O(1).",
-      "top_or_bottom": "bottom",
-      "occupied": false
-    },
-    {
-      "name": "Chuck Norris doesn't use a computer because a computer does everything slower than Chuck Norris.",
-      "top_or_bottom": "top",
-      "occupied": true
-    },
-    {
-      "name": "When Chuck Norris' code fails to compile the compiler apologises.",
-      "top_or_bottom": "bottom",
-      "occupied": false
-    },
-    {
-      "name": "There is nothing regular about Chuck Norris' expressions.",
-      "top_or_bottom": "top",
-      "occupied": true
-    },
-    {
-      "name": "Chuck Norris' addition operator doesn't commute; it teleports to where he needs it to be.",
-      "top_or_bottom": "bottom",
-      "occupied": false
-    },
-    {
-      "name": "When Chuck Norris' code fails to compile the compiler apologises.",
-      "top_or_bottom": "top",
-      "occupied": true
-    },
-    {
-      "name": "Chuck Norris rewrote the Google search engine from scratch.",
-      "top_or_bottom": "bottom",
-      "occupied": false
-    },
-    {
-      "name": "Chuck Norris doesn't need garbage collection because he doesn't call .Dispose(), he calls .DropKick().",
-      "top_or_bottom": "top",
-      "occupied": true
-    },
-    {
-      "name": "Chuck Norris doesn't pair program.",
-      "top_or_bottom": "bottom",
-      "occupied": false
-    },
-    {
-      "name": "All browsers support the hex definitions #chuck and #norris for the colors black and blue.",
-      "top_or_bottom": "top",
-      "occupied": true
-    },
-    {
-      "name": "Chuck Norris does not use exceptions when programming. He has not been able to identify any of his code that is not exceptional.",
-      "top_or_bottom": "bottom",
-      "occupied": false
-    },
-    {
-      "name": "Chuck Norris is immutable. If something's going to change, it's going to have to be the rest of the universe.",
-      "top_or_bottom": "top",
-      "occupied": true
-    },
-    {
-      "name": "Chuck Norris can write infinite recursion functions... and have them return.",
-      "top_or_bottom": "bottom",
-      "occupied": false
-    },
-    {
-      "name": "Chuck Norris' beard is immutable.",
-      "top_or_bottom": "top",
-      "occupied": true
-    },
-    {
-      "name": "Chuck Norris can access the DB from the UI.",
-      "top_or_bottom": "bottom",
-      "occupied": false
-    },
-    {
-      "name": "Chuck Norris can access private methods.",
-      "top_or_bottom": "top",
-      "occupied": true
-    },
-    {
-      "name": "There is nothing regular about Chuck Norris' expressions.",
-      "top_or_bottom": "bottom",
-      "occupied": false
-    },
-    {
-      "name": "Chuck Norris doesn't use a computer because a computer does everything slower than Chuck Norris.",
-      "top_or_bottom": "top",
-      "occupied": true
-    },
-    {
-      "name": "Chuck Norris can instantiate an abstract class.",
-      "top_or_bottom": "bottom",
-      "occupied": false
-    },
-    {
-      "name": "Chuck Norris doesn't program with a keyboard. He stares the computer down until it does what he wants.",
-      "top_or_bottom": "top",
-      "occupied": true
-    },
-    {
-      "name": "Chuck Norris can't test for equality because he has no equal.",
-      "top_or_bottom": "bottom",
-      "occupied": false
-    },
-    {
-      "name": "Chuck Norris can recite π. Backwards.",
-      "top_or_bottom": "top",
-      "occupied": true
-    },
-    {
-      "name": "Chuck Norris doesn't need garbage collection because he doesn't call .Dispose(), he calls .DropKick().",
-      "top_or_bottom": "bottom",
-      "occupied": false
-    },
-    {
-      "name": "Chuck Norris does not use revision control software. None of his code has ever needed revision.",
-      "top_or_bottom": "top",
-      "occupied": true
-    },
-    {
-      "name": "Chuck Norris is immutable. If something's going to change, it's going to have to be the rest of the universe.",
-      "top_or_bottom": "bottom",
-      "occupied": false
-    },
-    {
-      "name": "Chuck Norris doesn't need a java compiler, he goes straight to .war",
-      "top_or_bottom": "top",
-      "occupied": true
-    },
-    {
-      "name": "When Chuck Norris gives a method an argument, the method loses.",
-      "top_or_bottom": "bottom",
-      "occupied": false
-    },
-    {
-      "name": "Chuck Norris' programs occupy 150% of CPU, even when they are not executing.",
-      "top_or_bottom": "top",
-      "occupied": true
-    },
-    {
-      "name": "You don't disable the Chuck Norris plug-in, it disables you.",
-      "top_or_bottom": "bottom",
-      "occupied": false
-    },
-    {
-      "name": "Chuck Norris rewrote the Google search engine from scratch.",
-      "top_or_bottom": "top",
-      "occupied": true
-    },
-    {
-      "name": "All arrays Chuck Norris declares are of infinite size, because Chuck Norris knows no bounds.",
-      "top_or_bottom": "bottom",
-      "occupied": false
-    },
-    {
-      "name": "Chuck Norris's keyboard doesn't have a Ctrl key because nothing controls Chuck Norris.",
-      "top_or_bottom": "top",
-      "occupied": true
-    },
-    {
-      "name": "Chuck Norris can spawn threads that complete before they are started.",
-      "top_or_bottom": "bottom",
-      "occupied": false
-    },
-    {
-      "name": "Chuck Norris knows the last digit of PI.",
-      "top_or_bottom": "top",
-      "occupied": true
-    },
-    {
-      "name": "The only pattern Chuck Norris knows is God Object.",
-      "top_or_bottom": "bottom",
-      "occupied": false
-    },
-    {
-      "name": "There is nothing regular about Chuck Norris' expressions.",
-      "top_or_bottom": "top",
-      "occupied": true
-    },
-    {
-      "name": "Anonymous methods and anonymous types are really all called Chuck Norris. They just don't like to boast.",
-      "top_or_bottom": "bottom",
-      "occupied": false
-    },
-    {
-      "name": "Chuck Norris does not use exceptions when programming. He has not been able to identify any of his code that is not exceptional.",
-      "top_or_bottom": "top",
-      "occupied": true
-    },
-    {
-      "name": "Chuck Norris doesn't believe in floating point numbers because they can't be typed on his binary keyboard.",
-      "top_or_bottom": "bottom",
-      "occupied": false
-    },
-    {
-      "name": "Chuck Norris burst the dot com bubble.",
-      "top_or_bottom": "top",
-      "occupied": true
-    },
-    {
-      "name": "Chuck Norris can read all encrypted data, because nothing can hide from Chuck Norris.",
-      "top_or_bottom": "bottom",
-      "occupied": false
-    },
-    {
-      "name": "Chuck Norris can divide by zero.",
-      "top_or_bottom": "top",
-      "occupied": true
-    },
-    {
-      "name": "Chuck Norris can access private methods.",
-      "top_or_bottom": "bottom",
-      "occupied": false
-    },
-    {
-      "name": "Chuck Norris never gets a syntax error. Instead, the language gets a DoesNotConformToChuck error.",
-      "top_or_bottom": "top",
-      "occupied": true
-    },
-    {
-      "name": "Chuck Norris breaks RSA 128-bit encrypted codes in milliseconds.",
-      "top_or_bottom": "bottom",
-      "occupied": false
-    },
-    {
-      "name": "All browsers support the hex definitions #chuck and #norris for the colors black and blue.",
-      "top_or_bottom": "top",
-      "occupied": true
-    },
-    {
-      "name": "Whiteboards are white because Chuck Norris scared them that way.",
-      "top_or_bottom": "bottom",
-      "occupied": false
-    },
-    {
-      "name": "There is nothing regular about Chuck Norris' expressions.",
-      "top_or_bottom": "top",
-      "occupied": true
-    },
-    {
-      "name": "Chuck Norris burst the dot com bubble.",
-      "top_or_bottom": "bottom",
-      "occupied": false
-    },
-    {
-      "name": null,
-      "top_or_bottom": null,
-      "occupied": null
-    },
-    {
-      "name": "mdkwmkmd",
-      "top_or_bottom": "top",
-      "occupied": true
-    }
-  ],
- 
-  "table_headers": [
-    "name",
-    "top or bottom",
-    "occupied"  
-            
-  ]
-}; 
-
 $(function() {
   var tbody = $("<tbody />"), tr;
   $.each(bedsPayload.beds,function(_,obj) {
@@ -337,3 +12,41 @@ $(function() {
   });
   tbody.appendTo("#table_beds_payload");
 });
+
+var bedsPayload = {
+  "beds": [
+
+    {
+      "name": "John Booker",
+      "top_or_bottom": "bottom",
+      "occupied": false
+    },
+    {
+      "name": "betty careface",
+      "top_or_bottom": "bottom",
+      "occupied": false
+    },
+    {
+      "name": "Dan booler",
+      "top_or_bottom": "top",
+      "occupied": true
+    },
+    {
+      "name": "Chuck Norris",
+      "top_or_bottom": "bottom",
+      "occupied": false
+    },
+    {
+      "name": "Frank bool",
+      "top_or_bottom": "top",
+      "occupied": true
+    }
+  ],
+ 
+  "table_headers": [
+    "name",
+    "top or bottom",
+    "occupied"  
+            
+  ]
+}; 
