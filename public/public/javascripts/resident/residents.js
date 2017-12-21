1e2b3d26c495364c42a4641cde781e137c7f6a88
Troy Leach <troyleach29@gmail.com>
2017-12-20 21:38:02 -0600

    mergign branches together

--- public/public/javascripts/resident/residents.js
+++ public/public/javascripts/resident/residents.js
@@ -1,5 +1,5 @@
 function getAllResidents () {
-  var url = "http://localhost:3004/api/v1/residents";
+  var url = "http://localhost:3000/api/v1/residents";
   fetch(url)
     .then((resp) => resp.json())
     .then(function(data) {
@@ -17,15 +17,15 @@ function getAllResidents () {
         $.each(data.residents,function(_,obj) {
           rowWrapper = $("<div />").attr("id", "row").addClass("row-wrapper");
           tr = $(`<a href="residents/${obj.id}"></a>`).attr("id", "row").addClass("table-row");
-          tr.append(`<img src="${obj.image}" alt="" class="avatar-container">`);
+          tr.append(`<img src="${obj.image || "/images/default_avatar.png"}" alt="" class="avatar-container">`);
           tr.append("<div id='name' class='table-cell'> "+obj.first_name+ ' '+obj.last_name+" </div>");
           tr.append("<div class='table-cell'> "+obj.gender+" </div>");
           var date = new Date(obj.date);
           var rando = Math.floor((Math.random() * 3) + 1);
           tr.append("<div class='table-cell'> "+(date.getMonth()+1) + "/" + date.getDate() + "/" + date.getFullYear() +" </div>");
           tr.append("<div class='table-cell'> "+(date.getMonth()+1+rando) + "/" + date.getDate() + "/" + date.getFullYear() +" </div>");
-          tr.append("<div class='table-cell'> "+(obj.length_of_homelessness + " day(s)")+" </div>");
-          tr.append("<div class='table-cell'> "+obj.documented+" </div>");
+          tr.append(`<div class='table-cell'> ${obj.length_of_homelessness || "N/A"} day(s) </div>`);
+          tr.append(`<div class='table-cell'> ${obj.documented || "N/A"}</div>`);
           tr.append("<div class='table-cell'> "+obj.bed_id+" </div>");
           tr.appendTo(rowWrapper);
           rowWrapper.appendTo(tbody);
--- public/public/javascripts/resident/residentsNew.js
+++ public/public/javascripts/resident/residentsNew.js
@@ -1,7 +1,6 @@
 function submitResidentsForm() {
-    var url = "http://localhost:3004/api/v1/residents";
-
-    debugger
+  var url = "https://qa-san-jose.herokuapp.com/api/v1/residents";
+    //var url = "http://localhost:3000/api/v1/residents";
     var data = {
         resident: {
               first_name: document.forms.resident.first_name.value,
