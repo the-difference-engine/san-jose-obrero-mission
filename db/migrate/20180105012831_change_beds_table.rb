e1e93e0d43a79f39015f4b35d28d111e2ecfb70f
Bennett <bennett.lumbera@gmail.com>
2018-01-18 18:30:26 -0600

    fixed dropdown beds

--- db/migrate/20180105012831_change_beds_table.rb
+++
@@ -1,8 +0,0 @@
-class ChangeBedsTable < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :beds, :name, :string
-    remove_column :beds, :top_or_bottom, :string
-    rename_column :beds, :room_id, :bed_id
-    change_column :beds, :bed_id, :string
-  end
-end
--- public/public/javascripts/bed/beds.js
+++ public/public/javascripts/bed/beds.js
@@ -3,24 +3,20 @@
 //     fetch(url)
 //         .then((resp) => resp.json())
 //         .then(function(data){
-//             document.querySelector('#numOfBeds').innerHTML = data.beds.length;
+//             document.querySelector('#numOfBeds').innerHTML = data.length;
 //             $(function(){
 //                 var table = $("<div />").attr('id', 'bedsTable').addClass("bedsTable")
 //                 var thead = $("<span />").addClass("table-head");
 //                 var tr = $("<div />").addClass("head-table-row");
-//                 tr.append("<span class='table-cell-head'>Room Number</span>");
-//                 tr.append("<span class='table-cell-head'>Name</span>");
-//                 tr.append("<span class='table-cell-head'>Top or Bottom</span>");
+//                 tr.append("<span class='table-cell-head'>Bed Number</span>");
 //                 tr.append("<span class='table-cell-head'>Occupied</span>");
 //                 tr.append("<span class='table-cell-head'>Resident</span>");
 //                 tr.appendTo(thead);
 //                 var tbody = $("<div />").attr("id", "table-body").addClass("tbody");
-//                 $.each(data.beds,function(_,obj) {
+//                 $.each(data,function(_,obj) {
 //                     rowWrapper = $("<div />").attr("id", "table-body").addClass("tbody");
 //                     tr = $(`<a href="beds/${obj.id}"></a>`).attr("id", "row").addClass("table-row");
-//                     tr.append(`<div class='table-cell'>${obj.room_id || "N/A"}</div>`);
-//                     tr.append(`<div class='table-cell'>${obj.name || "N/A"}</div>`);
-//                     tr.append(`<div class='table-cell'>${obj.top_or_bottom || "N/A"}</div>`);
+//                     tr.append(`<div class='table-cell'>${obj.bed_id || "N/A"}</div>`);
 //                     tr.append(`<div class='table-cell'>${obj.occupied || "N/A"}</div>`);
 //                     tr.append(`<div class='table-cell'>${obj.resident_id || "N/A"}</div>`);
 //                     tr.appendTo(rowWrapper);
@@ -45,22 +41,30 @@ function getBeds() {
       $(function() {
         var label = $('<label for="bed_id">' + 'BED' + "</label>");
         var select = $('<select name="bed_id"></select>').addClass("form-control");
+        var full = true;
         for (var i = 0; i < data.length; i++) {
           if (data[i].occupied === false) {
-            var option = $(
-              select.append('<option value="${data[i].bed_id}">' + data[i].bed_id + '</option>')
-            );
-          } else {
-            option = $(
-              select.append('<option>' + 'None available' + '</option>')
-            );
+            full = false;
           }
+        }
+        if (full == false) {
+          for (var i = 0; i < data.length; i++) {
+            if (data[i].occupied === false) {
+              var option = $(
+                select.append('<option value=' + '"' + data[i].bed_id + '"' + '>' + data[i].bed_id + '</option>')
+              );
+            }
+          }
+        } else {
+          option = $(
+            select.append('<option>' + 'None available' + '</option>')
+          );
         }
         select.appendTo(label);
         option.appendTo(select);
         label.appendTo("#bed_id");
-        
       });
     });
 }
+
 getBeds();
